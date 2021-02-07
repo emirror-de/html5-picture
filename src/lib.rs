7f9bc287f96ac4b9f7972a9bf86c329beb1d2fcd
Lewin Probst <info@emirror.de>
2021-02-07 23:12:32 +0100

    Added examples in documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! # Purpose and usage
-//! 
+//!
 //! Supports the binary within this package.
 //! Contains functions to easily generate different sizes of a picture that
 //! is used on webpages. Also offers the possibility to convert them into webp
@@ -14,15 +14,24 @@
 //! The binary can be installed via ```cargo install html5-picture```. As stated
 //! before, make sure webp is installed before using.
 
-
 /// Support for webp format. Used mainly for conversion.
 pub mod webp;
 
-use {
-    std::path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
 
-/// Generates the output directory name.
+/// Generates the output directory name by attaching a fixed string to it.
+/// ## Example
+///
+/// ```
+/// use {
+///     html5_picture::get_output_dir_name,
+///     std::path::PathBuf,
+/// };
+///
+/// let input = PathBuf::from("../assets");
+/// let input = get_output_dir_name(&input).unwrap();
+/// assert_eq!(input.to_str().unwrap(), "../assets-html5picture");
+/// ```
 pub fn get_output_dir_name(input_dir: &PathBuf) -> Result<PathBuf, String> {
     // validity checks
     if !&input_dir.is_dir() {
@@ -32,7 +41,9 @@ pub fn get_output_dir_name(input_dir: &PathBuf) -> Result<PathBuf, String> {
         }
     }
     if let None = &input_dir.file_name() {
-        return Err(String::from("The last segment of the input path is not valid!"));
+        return Err(String::from(
+            "The last segment of the input path is not valid!",
+        ));
     };
     // get input directory name
     let input_dir_name = input_dir.file_name().unwrap();
@@ -42,22 +53,34 @@ pub fn get_output_dir_name(input_dir: &PathBuf) -> Result<PathBuf, String> {
         None => Path::new(""),
     };
     // generate output directory
-    Ok(parent.join(format!("{}-html5picture", input_dir_name.to_str().unwrap())))
+    Ok(parent
+        .join(format!("{}-html5picture", input_dir_name.to_str().unwrap())))
 }
 
 /// Removes the given base directory from the given input file.
-pub fn remove_base_dir(base_dir: &PathBuf, input_file: &PathBuf) -> Result<PathBuf, String> {
+/// ## Example
+///
+/// ```
+/// use {
+///     html5_picture::get_output_dir_name,
+///     std::path::PathBuf,
+/// };
+///
+/// let input = PathBuf::from("../assets");
+/// let input = html5_picture::get_output_dir_name(&input).unwrap();
+/// assert_eq!(input.to_str().unwrap(), "../assets-html5picture");
+/// ```
+pub fn remove_base_dir(
+    base_dir: &PathBuf,
+    input_file: &PathBuf,
+) -> Result<PathBuf, String> {
     match pathdiff::diff_paths(input_file, base_dir) {
         Some(p) => Ok(p),
-        None => {
-            match input_file.to_str() {
-                Some(v) => Err(
-                    String::from(
-                        format!("Could not remove base dir of {}", v)
-                        )
-                    ),
-                None => Err(String::from("Could not remove base dir!")),
+        None => match input_file.to_str() {
+            Some(v) => {
+                Err(String::from(format!("Could not remove base dir of {}", v)))
             }
+            None => Err(String::from("Could not remove base dir!")),
         },
     }
 }
@@ -77,13 +100,11 @@ pub fn create_output_dir(input_dir: &PathBuf) -> Result<(), String> {
 /// Determines if the given input filename contains a .png extension.
 pub fn is_png(input: &PathBuf) -> bool {
     match input.extension() {
-        Some(s) => {
-            match s.to_str() {
-                None => false,
-                Some(v) => v == "png",
-            }
+        Some(s) => match s.to_str() {
+            None => false,
+            Some(v) => v == "png",
         },
-        None => false
+        None => false,
     }
     //input.ends_with(".png")
 }
