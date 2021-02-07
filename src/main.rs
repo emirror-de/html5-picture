0cbcd6962890b45a74f06a425d0064c6b0e68abc
Lewin Probst <info@emirror.de>
2021-02-07 23:34:37 +0100

    Refactoring, added quality option for webp conversion.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #[warn(missing_docs)]
-
 use {
     clap::Clap,
+    html5_picture::webp,
     log::error,
-    walkdir::WalkDir,
     std::path::PathBuf,
+    walkdir::WalkDir,
 };
+
+const DEFAULT_QUALITY_WEBP: u8 = 70;
 
 /// Scales the input images (currently png only) to the given breakpoints and
 /// converts them to webp format.
@@ -16,9 +18,14 @@ use {
 struct Args {
     /// The directory containing all images that should be processed.
     pub input_dir: PathBuf,
+    /*
     /// Count of scaled images to be calculated.
     #[clap(short)]
     pub scaled_images_count: Option<u8>,
+    */
+    /// Defines the quality of cwebp conversion.
+    #[clap(short)]
+    pub quality_webp: Option<u8>,
     /*
     /// Disable conversion to webp. (Not implemented yet)
     #[clap(short)]
@@ -26,24 +33,49 @@ struct Args {
     */
 }
 
+fn check_arguments(config: &mut Args) {
+    // set default quality
+    if let None = &config.quality_webp {
+        config.quality_webp = Some(DEFAULT_QUALITY_WEBP);
+    } else {
+        // only a quality between 1 and 100 is available for webp
+        config.quality_webp = if config.quality_webp.unwrap() > 100 {
+            Some(100)
+        } else if config.quality_webp.unwrap() < 1 {
+            Some(1)
+        } else {
+            Some(DEFAULT_QUALITY_WEBP)
+        };
+    }
+}
+
 fn main() {
     pretty_env_logger::init();
 
-    let config: Args = Args::parse();
+    // parse and check arguments for validity
+    let mut config: Args = Args::parse();
+    check_arguments(&mut config);
+    let config = config;
 
     // get overall output directory
-    let output_base_dir = match html5_picture::get_output_dir_name(&config.input_dir) {
-        Ok(o) => o,
-        Err(msg) => {
-            error!("{}", msg);
-            return;
-        },
-    };
+    let output_base_dir =
+        match html5_picture::get_output_dir_name(&config.input_dir) {
+            Ok(o) => o,
+            Err(msg) => {
+                error!("{}", msg);
+                return;
+            }
+        };
     // create output directory for converted files
     if let Err(msg) = html5_picture::create_output_dir(&config.input_dir) {
         error!("{}", msg);
         return;
     }
+
+    // Instantiate converter
+    let webp_converter = webp::WebpConverter {
+        quality: config.quality_webp.unwrap(),
+    };
 
     // process all images
     for entry in WalkDir::new(&config.input_dir) {
@@ -57,15 +89,13 @@ fn main() {
 
         let entry = entry.into_path();
         // get resulting output path name
-        let f = match html5_picture::remove_base_dir(
-            &config.input_dir,
-            &entry
-            ) {
+        let f = match html5_picture::remove_base_dir(&config.input_dir, &entry)
+        {
             Ok(s) => s,
             Err(msg) => {
                 error!("{}", msg);
                 return;
-            },
+            }
         };
         let resulting_output_path = output_base_dir.join(f);
 
@@ -75,21 +105,23 @@ fn main() {
                 Ok(()) => (),
                 Err(msg) => {
                     match resulting_output_path.to_str() {
-                        Some(v) => error!("Could not create folder {}! Error: {}", v, msg),
-                        None => error!("Could not create folder! Error {}", msg),
+                        Some(v) => error!(
+                            "Could not create folder {}! Error: {}",
+                            v, msg
+                        ),
+                        None => {
+                            error!("Could not create folder! Error {}", msg)
+                        }
                     };
                     return;
-                },
+                }
             };
         }
 
         // check if entry is a png file
         if html5_picture::is_png(&resulting_output_path) {
-            html5_picture::webp::convert_from_png(
-                &PathBuf::from(entry),
-                &resulting_output_path
-                );
+            webp_converter
+                .from_png(&PathBuf::from(entry), &resulting_output_path);
         }
     }
-
 }
--- src/webp.rs
+++ src/webp.rs
@@ -1,37 +1,54 @@
 use {
-    log::{info, error},
-    std::path::PathBuf
+    log::{error, info},
+    std::path::PathBuf,
 };
 
-/// Converts the given input file to webp using ```cwebp``` and saves it to disk
-/// into the output_dir folder.
-pub fn convert_from_png(input_file_name: &PathBuf, output_file_name: &PathBuf) {
-    if !&input_file_name.is_file() {
-        error!("{} is not a file!", &input_file_name.to_str().unwrap());
-        return;
-    }
+pub struct WebpConverter {
+    pub quality: u8,
+}
 
-    if output_file_name.is_dir() {
-        error!("{} is a directory!", &output_file_name.to_str().unwrap());
-        return;
-    };
-    let o = &mut output_file_name.clone();
-    o.set_extension("webp");
+impl WebpConverter {
+    /// Converts the given input file to webp using ```cwebp``` and saves it to disk
+    /// into the output_dir folder.
+    pub fn from_png(
+        &self,
+        input_file_name: &PathBuf,
+        output_file_name: &PathBuf,
+    ) {
+        if !&input_file_name.is_file() {
+            error!("{} is not a file!", &input_file_name.to_str().unwrap());
+            return;
+        }
 
-    let process = match std::process::Command::new("cwebp")
-        .args(&[input_file_name.to_str().unwrap(), "-q", "100", "-o", o.to_str().unwrap()])
-        .spawn() {
+        if output_file_name.is_dir() {
+            error!("{} is a directory!", &output_file_name.to_str().unwrap());
+            return;
+        };
+        let o = &mut output_file_name.clone();
+        o.set_extension("webp");
+
+        let process = match std::process::Command::new("cwebp")
+            .args(&[
+                input_file_name.to_str().unwrap(),
+                "-q",
+                &self.quality.to_string(),
+                "-o",
+                o.to_str().unwrap(),
+            ])
+            .spawn()
+        {
             Ok(process) => process,
-            Err(err)    => panic!("Running process error: {}", err),
-    };
+            Err(err) => panic!("Running process error: {}", err),
+        };
 
-    let output = match process.wait_with_output() {
-            Ok(output)  => output,
-            Err(err)    => panic!("Retrieving output error: {}", err),
-    };
+        let output = match process.wait_with_output() {
+            Ok(output) => output,
+            Err(err) => panic!("Retrieving output error: {}", err),
+        };
 
-    match std::string::String::from_utf8(output.stdout) {
-            Ok(stdout)  => info!("{}", stdout),
-            Err(err)    => panic!("Translating output error: {}", err),
-    };
+        match std::string::String::from_utf8(output.stdout) {
+            Ok(stdout) => info!("{}", stdout),
+            Err(err) => panic!("Translating output error: {}", err),
+        };
+    }
 }
